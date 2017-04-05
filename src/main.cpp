252a8043294c074fce7586435281ad85e42eeb56
Nico De Witte <nico.dewitte@vives.be>
2017-04-05 17:47:41 +0200

    Add simple time based tracking for potty

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,37 @@
 #include "lib/bios_opencv/frame_grabber/image_file.h"
 #include "lib/bios_opencv/filter_chain/filter_chain.h"
 #include "lib/bios_opencv/filter/all_filters.h"
+#include <ctime>
 
 using namespace BiosOpenCV;
+
+
+// This should go into class of its own
+bool pottyIsBeingTracked = false;
+
+double startTime = 0;
+double timeTracked = 0;
+double timeLost = 0;
+
+void indicate_potty_is_tracked(void) {
+  if (!pottyIsBeingTracked) {
+    timeLost = double(cv::getTickCount()-startTime) / double(cv::getTickFrequency());
+    std::cout << "Tracking potty - Lost for " << timeLost << " seconds" << std::endl;
+    startTime = cv::getTickCount();
+  }
+
+  pottyIsBeingTracked = true;
+}
+
+void indicate_potty_is_lost(void) {
+  if (pottyIsBeingTracked) {
+    timeTracked = double(cv::getTickCount()-startTime) / double(cv::getTickFrequency());
+    std::cout << "Lost potty - Tracked for " << timeTracked << " seconds" << std::endl;
+    startTime = cv::getTickCount();
+  }
+
+  pottyIsBeingTracked = false;
+}
 
 int main(int argc, const char * argv[])
 {
@@ -42,13 +71,40 @@ int main(int argc, const char * argv[])
   filters.add(new GrabFrame(original, &frameGrabber));
   filters.add(new GrayScale(original, grayscale));
   filters.add(new BinaryThreshold(grayscale, processed, 140));
-  filters.add(new FindContours(processed, original, 400, 500));
-  filters.add(new MatchTemplate(grayscale, original, pottyTemplate));
+
+  FindContours findContours(processed, original, 400, 500);
+  filters.add(&findContours);
+
+  MatchTemplate templateMatcher(grayscale, original, pottyTemplate);
+  filters.add(&templateMatcher);
+
   filters.add(new Display(original, "original"));
 
   do {
     if (!paused || step) {
       filters.execute();
+
+      cv::Point2f matchLocation;
+      matchLocation.x = templateMatcher.get_match_center().x;
+      matchLocation.y = templateMatcher.get_match_center().y;
+
+      std::vector<cv::Point2f> centers = findContours.get_centers();
+
+      bool foundPotty = false;
+      for (size_t i = 0; i < centers.size(); i++) {
+        std::cout << "Distance [" << i << "] = " << (cv::norm(centers[i] - matchLocation)) << std::endl;
+        if (cv::norm(centers[i] - matchLocation) <= 25) {
+          std::cout << "Got match for " << i << " between template and blobs" << std::endl;
+          foundPotty = true;
+          indicate_potty_is_tracked();
+          break;
+        }
+      }
+
+      if (!foundPotty) {
+        indicate_potty_is_lost();
+      }
+
       step = false;
     }
 
