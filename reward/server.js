3d3f048f6b4b922ba2551f31316b78243945bd5f
Nico De Witte <nico.dewitte@vives.be>
2017-04-06 11:27:29 +0200

    Extreme simple reward using applause sound

---
+++ reward/server.js
@@ -0,0 +1,27 @@
+var player = require('play-sound')(opts = {})
+
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://mqtt.labict.be')
+
+var garageState = ''
+var connected = false
+
+client.on('connect', () => {
+  client.subscribe('pottydetector/timer')
+})
+
+client.on('message', (topic, message) => {
+  switch (topic) {
+    case 'pottydetector/timer':
+      return handleTimer(message)
+  }
+  console.log('No handler for topic %s', topic)
+})
+
+function handleTimer (message) {
+  console.log('timer: %s', message)
+
+  player.play('yeehaw.mp3', function(err){
+    if (err) throw err
+  })
+}
